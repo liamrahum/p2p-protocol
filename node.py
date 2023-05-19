7858fb1cb193a48124178f924bbe0361675fea39
Liam Rahum <liam.rahum@gmail.com>
2023-05-19 10:35:47 +0300

    Feat: added client functionality to node

--- node.py
+++ node.py
@@ -4,7 +4,9 @@ import threading
 # threading.Thread(target=thread_function, args=(1,))
 # x.start()
 
-SERVER_IP = ''
+SERVER_IP = '192.168.1.2'
+SERVER_PORT = 5305
+
 MY_IP = '127.0.0.1'
 MY_PORT = int(input("Enter node port: "))
 
@@ -19,28 +21,46 @@ class ClientThread(threading.Thread):
         self.ip = ip
         self.port = port
         self.conn = conn
+        print ("[+] New client socket thread started for " + ip + ":" + str(port))
+    
+    def run(self): 
+        try:
+            while True:
+                data = self.conn.recv(1024)
+                print("Client sent data:", data.decode())
+                self.conn.sendall("kova".encode())
+        except Exception as e:
+            print("Client probably disconnected")
+
+class ServerThread(threading.Thread):
+    def __init__(self,ip,port, conn): 
+        threading.Thread.__init__(self) 
+        self.ip = ip
+        self.port = port
+        self.conn = conn
         print ("[+] New server socket thread started for " + ip + ":" + str(port))
     
     def run(self): 
         try:
             while True:
                 data = self.conn.recv(1024)
-                print("Server received data:", data.decode())
+                print("Server sent data:", data.decode())
                 self.conn.sendall("kova".encode())
         except Exception as e:
-            print("Client probably disconnected")
-
-def send_recv_msg(sock):
-    
-    return
+            print("Server probably disconnected")
 
 def main():
     listening_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (MY_IP, MY_PORT)
-    listening_sock.bind(server_address)
+    my_address = (MY_IP, MY_PORT)
+    listening_sock.bind(my_address)
     listening_sock.listen(4)
     threads = []
     
+    server_socket = socket.socket()  # instantiate
+    server_socket.connect((SERVER_IP, SERVER_PORT))  # connect to the server
+    newthread = ServerThread(SERVER_IP,SERVER_PORT,server_socket)
+    newthread.start()
+
     while True:
         try:
             listening_sock.listen(1)
@@ -48,9 +68,8 @@ def main():
             newthread = ClientThread(ip,port, conn)
             newthread.start()
             threads.append(newthread)
-            newthread.daemon()
         except Exception as e:
             continue
-
+    client_socket.close()  # close the connection
 if __name__ == '__main__':
     main()
